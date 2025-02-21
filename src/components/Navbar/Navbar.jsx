08027f9622376be6d911ad1817771a430ee57fca
hk265 <Nimcet9900@gmail.com>
2025-02-21 18:46:23 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    
+    <nav className="container">
+        <div className="logo">
+            <img src="/images/logo.webp" width={70} alt="" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar 
